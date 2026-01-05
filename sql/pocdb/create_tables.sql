0fb691cdfd99771e9f08d19605f639bef944f241
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-01-05 20:54:31 +0530

    Create create_tables.sql

---
+++ sql/pocdb/create_tables.sql
@@ -0,0 +1,11 @@
+USE pocdb;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100)
+);
+
+INSERT INTO users (username, email) VALUES
+('mariyababu', 'mariyababu@enreap.com'),
+('avinash', 'avinash@enreap.com');
