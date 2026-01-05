f156a2d65ce7ae47724af43851227057b6db7c26
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-01-05 21:40:11 +0530

    Update create_tables.sql

--- sql/database1/create_tables.sql
+++ sql/database1/create_tables.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (username, email) VALUES
-('alice', 'alice@example.com'),
+('mariyababudasari', 'mariyababu@example.com'),
 ('bob', 'bob@example.com');
